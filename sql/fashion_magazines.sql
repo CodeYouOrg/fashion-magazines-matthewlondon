eff4085fbaef6842d9e34f6ceb1de7f2698b90e0
Matthew London <mmatthewlondon@gmail.com>
2024-10-20 10:48:17 -0400

    Filtered Unpaid Fashoin dues

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,10 @@
--- Add your sql here
+SELECT 
+customers.customer_name AS 'Customer', 
+printf('$%.2f',SUM(subscription_length * price_per_month)) AS 'Amount Due'
+FROM orders
+JOIN customers
+    on customers.customer_id = orders.customer_id
+JOIN subscriptions
+    on subscriptions.subscription_id = orders.subscription_id
+WHERE orders.order_status = 'unpaid' AND subscriptions.description = 'Fashion Magazine'
+GROUP BY customers.customer_name;
